64492c04fc54188b2561d010117c04249504439d
Ben <bennnnnnnvu@gmail.com>
2025-05-29 00:08:51 -0700

    add code to handle uncaughtException and unhandledRejection errors, as well as SIGTERM signals to close the server gracefully

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,43 @@ dotenv.config();
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, () => {
+
+let server;
+server = app.listen(PORT, () => {
     logger.info(`server is listening on port ${PORT}!!!`);
 });
+
+///////     handle server errors    ////////
+
+interface ServerError extends Error {
+    error: string | number;
+};
+
+const closeServer = () => {
+    if(server) {
+        logger.info('Server closed.');
+        process.exit(1)  // kill server. Exiting with "1" means there was some sort of problem. Exiting with 0 means no issues occurred
+    } else {
+        process.exit(1)
+    }
+};
+
+const unexpectedErrorHandler = (error: ServerError) => {
+    logger.error(error);
+    closeServer();
+};
+
+//handle uncaughtExceptipn + unhandledRejection errors
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+//SIGTERM (signal 15) is used in Linux to terminate a process gracefully
+//.on is an event listener. Here we are just saying if we receive a sigterm and the server is currently running, then just close it
+process.on("SIGTERM", () => {
+    if(server) {
+        logger.info("Server closed.");
+        process.exit(1);
+    }
+});
+///////    ***END*** handle server errors  ***END***  ////////
