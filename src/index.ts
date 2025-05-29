cb26c2dfbb75dd8fd66d72ecc73f7c679a63298f
Ben <bennnnnnnvu@gmail.com>
2025-05-29 00:53:00 -0700

    add connection string to mongoDB atlas cluster in .env file, then connect to cloud database cluster in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
 import app from "./app";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import logger from "./configs/winston-logger";
 
 //dotEnv config
 dotenv.config();
 
-// console.log(process.env.NODE_ENV);
-
+// use environment variables
 const PORT = process.env.PORT || 6000;
+const { DATABASE_URL } = process.env;
 
+//enable mongoDB debug mode for development
+if(process.env.NODE_ENV !== "production"){
+    mongoose.set("debug", true);
+};
 
+//establish a connection to our mongoDB atlas cluster
+const connectToDB = async () => {
+    try {
+        if(!process.env.DATABASE_URL || !DATABASE_URL || DATABASE_URL.length === 0){
+            logger.error(`DATABASE_URL environment variable is either missing or undefined!`);
+        } else {
+            await mongoose.connect(DATABASE_URL);
+            logger.info(`Successfully connected to MongoDB Atlas cloud database!`);
+        }
+
+    } catch(error){
+        logger.error(`Errpr connecting to MongoDB Atlas cluster! : ${error}`);
+        process.exit(1);
+    }
+};
+
+connectToDB();
+
+// ******    set up the server     ******
 let server;
 server = app.listen(PORT, () => {
     logger.info(`server is listening on port ${PORT}!!!`);
 });
 
-///////     handle server errors    ////////
+//*****     handle server errors    *****
 
 interface ServerError extends Error {
     error: string | number;
@@ -48,4 +72,4 @@ process.on("SIGTERM", () => {
         process.exit(1);
     }
 });
-///////    ***END*** handle server errors  ***END***  ////////
+//***END*** handle server errors  ***END***
